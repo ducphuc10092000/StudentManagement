857bbbf929f9abfe2aa63b881cd298c249bfec7f
Nguyen Duc Phuc <ducphuc10092000@github.com>
2021-05-23 09:47:50 +0700

    Create Structure of Project

---
+++ StudentManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace StudentManagement.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Chức năng Button chuyển UC
+        public enum CHUCNANG
+        {
+            DashBoard, ManageStudent, ManageTeacher, ManageClass, ManageRoom, ManageMark, ManageFee
+        }
+        private int _ChucNang;
+        public int ChucNang { get => _ChucNang; set { _ChucNang = value; OnPropertyChanged(); } }
+        #endregion
+
+        #region Declare Binding Command
+        public ICommand QuitCommand { get; set; }
+
+        public ICommand BtnDashBoardCommand { get; set; }
+        public ICommand BtnStudentCommand { get; set; }
+        
+
+        #endregion
+
+        public MainViewModel()
+        {
+            #region Handle Binding Command Swap UserControl
+            BtnDashBoardCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.DashBoard;
+            });
+            BtnStudentCommand = new RelayCommand<object>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                ChucNang = (int)CHUCNANG.ManageStudent;
+            });
+            #endregion
+            #region Handle Binding Command
+            QuitCommand = new RelayCommand<MainWindow>((p) =>
+            {
+                //if (AccountPower == 0 || AccountPower == 1)
+                //{
+                //    MessageBoxResult result = MessageBox.Show("Bạn không đủ quyền truy cập vào chức năng này!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //    return false;
+                //}
+
+
+                return true;
+            }, (p) =>
+            {
+                MessageBoxResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát?", "Thông báo", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    p.Close();
+                }
+                else
+                {
+                    return;
+                }
+            });
+            #endregion
+        }
+    }
+}
